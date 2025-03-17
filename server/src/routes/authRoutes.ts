7f45a95498c3c26a9b01740139086cc4a78a9e02
AfeeqaMuzammil <hanaafeeqa279@gmail.com>
2025-03-17 23:56:07 +0530

    implement reset password code for server

--- server/src/routes/authRoutes.ts
+++ server/src/routes/authRoutes.ts
@@ -1,10 +1,11 @@
 import express from 'express';
-import { register } from '../controllers/authController';
+import { register, getMe } from '../controllers/authController';
 import { protect } from '../middleware/auth';
 
 const router = express.Router();
 
 router.post('/register', register);
+router.get('/me', protect, getMe);
 
 
 export default router;
